36e0382f5470456e1255621dcdb7cd6bc136c480
User 12 <nirbhey.012@cloudcounselage.com>
2021-04-16 21:15:34 +0530

    fixed links,images,counter

--- src/main.js
+++ src/main.js
@@ -14,3 +14,13 @@ let nCount = (selector) => {
     );
   });
 };
+
+let a = 0;
+$(window).scroll(function () {
+  // The .offset() method allows us to retrieve the current position of an element  relative to the document
+  let oTop = $(".numbers").offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() >= oTop) {
+    a++;
+    nCount(".rect > h1");
+  }
+});
